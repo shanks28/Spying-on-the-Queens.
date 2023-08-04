6ca62ae0537ff129e46f3ac0f550d36e23e2f707
gold_roger <1rn21cs016.akash@gmail.com>
2023-08-04 18:42:47 +0530

    The row label now appears now for the column and the adjustments

--- GUI.py
+++ GUI.py
@@ -1,7 +1,10 @@
-import pygame as p
 import tkinter as tk
+
+import pygame as p
+window=0
 black = (0, 0, 0)
 white = (255, 255, 255)
+blue=(255,0,0)
 choice_4_queens_button=None
 def choice_window():
     global choice_4_queens_button
@@ -26,26 +29,37 @@ def draw_4_queens(window,square_size):
                 p.draw.rect(window, black, (pos_x, pos_y, square_size, square_size))
             else:
                 p.draw.rect(window, white, (pos_x, pos_y, square_size, square_size))
+
+def draw_label_for_board(window_dimensions,square_size):
+    global window
+    for index,element in enumerate([1,2,3,4][::-1]):
+        font = p.font.Font(None, 40)
+        row_number_text=font.render(str(element),True,blue) # this function takes the string to be rendered , a boolean asking ifyou want antialiasing and athe color of the string that is to be rendered
+        window.blit(row_number_text,(window_dimensions[0]-square_size,index*square_size+square_size//2))
+
 def draw_pygame_window():
+    global window
     choice_4_queens_button.config(state=tk.DISABLED)
-    window_dimensions = [800, 800]
+    window_dimensions = [820, 820]
+    p.display.set_caption("This the Caption")
     p.init()  # when we call this function we essentially call the lower level p.display.init() function
     flags = p.RESIZABLE | p.HWSURFACE | p.DOUBLEBUF
     window = p.display.set_mode((window_dimensions[0], window_dimensions[1]), flags)
-    rows = [1, 2, 3, 4, 5, 6, 7, 8]
+    rows = [1, 2, 3, 4]
     columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    square_size = (window_dimensions[0] / 4)-5
+
     pos_x, pos_y = (0, 0)
-    square_size = (window_dimensions[0] / 4)
     clock = p.time.Clock()
     running = True
-    while (running):
+    while (running): # this is also called as the game loop
         for event in p.event.get():
             if (event.type == p.QUIT):
                 running = False
-        draw_4_queens(window,square_size)
-        p.display.flip()
         window.fill(white)
-        clock.tick(60)
+        draw_4_queens(window, square_size)
+        draw_label_for_board(window_dimensions, square_size)
+        p.display.flip()
     p.display.quit()  # this is already handled when the program exits and is harmless to call it again this is to just create an exit point for my function
     choice_4_queens_button['state']='normal'
 if(__name__=="__main__"):
